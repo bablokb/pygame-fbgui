a02f59ed30cd123f9f147b848448eff6e2ea69de
Bernhard Bablok <bablokb@gmx.de>
2019-01-18 19:55:55 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Package fbgui with basic GUI-elements for a simple framebuffer application.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-wstation
+#
+# ----------------------------------------------------------------------------
+
+from Settings import Settings as Settings
+
+from App      import App      as App
+from Widget   import Widget   as Widget
+
