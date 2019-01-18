f51b3309357e03b09c2cffb12fdfb6d34e071f13
Bernhard Bablok <bablokb@gmx.de>
2019-01-18 19:42:37 +0100

    moved to package fbgui

---
+++ App.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Application based on pygame.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-wstation
+#
+# ----------------------------------------------------------------------------
+
+import os
+
+import pygame
+import pygame.freetype
+import pygame.gfxdraw
+
+from Settings import Settings
+
+class App(object):
+  """ Application based on pygame """
+
+  # --- constructor   --------------------------------------------------------
+  
+  def __init__(self,settings=Settings()):
+    "Ininitializes a new pygame screen using the framebuffer"
+
+    self.screen  = None
+    self.display = Settings({
+      'title':     "Application-Title",
+      'width':     800,
+      'height':    600,
+      'size':      (800,600),
+      'font_name': "FreeSans.ttf",
+      'font_size': 12})
+    self.display.copy(settings)
+
+    self._init_screen(settings)
+    self._init_font()
+    pygame.fastevent.init()
+
+  # --- initialize pygame screen   -------------------------------------------
+  
+  def _init_screen(self,settings):
+    """ initialize pygame screen
+
+      Based on 'Python GUI in Linux frame buffer'
+      http://www.karoltomala.com/blog/?p=679
+    """
+    
+    if hasattr(settings,"fb_device"):
+      os.environ["SDL_FBDEV"] = settings.fb_device
+    else:
+      os.environ["SDL_FBDEV"] = "/dev/fb1"
+    have_X = os.getenv("DISPLAY")
+
+    if have_X:
+      pygame.display.init()
+      self.display.size   = (self.display.width,self.display.height)
+      self.screen         = pygame.display.set_mode(self.display.size)
+      if self.display.title:
+        pygame.display.set_caption(self.display.title)
+    else:
+      # Check which frame buffer drivers are available
+      # Start with fbcon since directfb hangs with composite output
+      drivers = ['fbcon', 'directfb', 'svgalib']
+      found = False
+      for driver in drivers:
+        # Make sure that SDL_VIDEODRIVER is set
+        if not os.getenv('SDL_VIDEODRIVER'):
+          os.putenv('SDL_VIDEODRIVER', driver)
+          try:
+            pygame.display.init()
+            found = True
+            break
+          except pygame.error:
+            print 'Driver: {0} failed.'.format(driver)
+    
+        if not found:
+          raise Exception('No suitable video driver found!')
+        
+        self.display.size   = (pygame.display.Info().current_w,
+                               pygame.display.Info().current_h)
+        self.display.width  = self.display.size[0]
+        self.display.height = self.display.size[1]
+        self.screen = pygame.display.set_mode(self.display.size,
+                                              pygame.FULLSCREEN)
+
+  # --- initialize font support   --------------------------------------------
+
+  def _init_font(self):
+    """ initialize font support """
+
+    pygame.freetype.init()
+    self.default_font = pygame.freetype.SysFont(self.display.font_name,
+                                                self.display.font_size)
+
+  # --- main event loop   -----------------------------------------------------
+
+  def run(self):
+    """ main event loop """
+    
+    while True:
+      event = pygame.fastevent.wait()
+      if event.type == pygame.QUIT:
+        return
+
+      self.screen.fill((0,0,255))
+      pygame.display.flip()
+
+  # --- terminate application   ----------------------------------------------
+
+  def quit(self):
+    """ terminate application """
+    pygame.quit()
---
+++ Settings.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# A simple value-holder class for settings.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-wstation
+#
+# ----------------------------------------------------------------------------
+
+class Settings(object):
+  """ Value-holder class for arbitrary settings """
+
+  # --- constructor   --------------------------------------------------------
+  
+  def __init__(self,defaults=None):
+    """ constructor:
+        pass a list of 'supported' keys in a dict with  default values
+    """
+    if defaults:
+      self._keys = defaults.keys()
+      for key in self._keys:
+        setattr(self,key,defaults[key])
+
+  # --- copy keys from other settings-object   -------------------------------
+  
+  def copy(self,other):
+    """ copy keys from other settings """
+
+    # extract keys
+    if self._keys:
+      keys = self._keys
+    else:
+      keys = [d for d in other.__dict__
+                             if not callable(d) and not d.startswith("_")]
+    
+    # copy relevant keys
+    for key in keys:
+      if hasattr(other,key):
+        setattr(self,key,getattr(other,key))
