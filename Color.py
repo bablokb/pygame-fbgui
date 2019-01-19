2b1c32e5d2663740eee182d84988fbe48470eb44
Bernhard Bablok <bablokb@gmx.de>
2019-01-19 12:11:27 +0100

    initial commit

---
+++ Color.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# A class for color-constants
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-wstation
+#
+# ----------------------------------------------------------------------------
+
+import pygame
+
+class Color(object):
+  """ Color constants """
+
+  WHITE = pygame.Color("white")
+  BLACK = pygame.Color("black")
+  RED   = pygame.Color("red")
+  GREEN = pygame.Color("green")
+  BLUE  = pygame.Color("blue")
