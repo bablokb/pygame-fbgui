c0bb6cfdb360cb4e34f0cdd2da6c00aef2e859b2
Bernhard Bablok <bablokb@gmx.de>
2019-01-26 11:17:33 +0100

    initial commit

---
+++ VBox.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Class VBox: a panel with a vertical layout of it's children
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-wstation
+#
+# ----------------------------------------------------------------------------
+
+import fbgui
+
+class VBox(fbgui.Box):
+  """ Horizontal box """
+
+  # --- constructor   --------------------------------------------------------
+  
+  def __init__(self,id,settings=None,toplevel=False):
+    """ constructor """
+
+    super(VBox,self).__init__(id,settings=settings,toplevel=toplevel)
+
+  # --- query minimum size   -------------------------------------------------
+
+  def _minimum_size(self,w,h):
+    """ query minimum size of widget """
+
+    if self.w_min  > 0 and self.h_min > 0:
+      return (self.w_min,self.h_min)
+
+    # minimum size is either absolute, relative to parent or
+    #  - w_max of childs + margins
+    #  - h_sum + (n-1)*padding + margins
+
+    # size without children (panel size incl. margins)
+    (self.w_min,self.h_min) = super(VBox,self)._minimum_size(w,h)
+
+    n_childs = len(self._childs)
+    if not n_childs:
+      fbgui.App.logger.msg("DEBUG",
+                  "min_size (%s): (%d,%d)" % (self._id,self.w_min,self.h_min))
+      return (self.w_min,self.h_min)
+      
+    # child-dimensions
+    self._get_sizes(w,h)
+    self.h_min = max(self.h_min,self._child_h_sum +
+                              (n_childs-1)*self.padding[0] +
+                                         self.margins[2]+self.margins[3])
+    self.w_min = max(self.w_min,
+                     self._child_w_max + self.margins[0]+self.margins[1])
+
+    fbgui.App.logger.msg("DEBUG",
+              "min_size (%s): (%d,%d)" % (self._id,self.w_min,self.h_min))
+    return (self.w_min,self.h_min)
+
+  # --- layout widget   ------------------------------------------------------
+
+  def _layout(self,x,y,w,h):
+    """ layout widget """
+
+    if self._is_layout:
+      return                                # layout already done
+
+    # do default behaviour (sets self._is_layout to True)
+    self._std_layout(x,y,w,h)
+
+    # correct values for margins
+    x = x + self.margins[0]
+    y = y + self.margins[2]
+    w = w - self.margins[0] - self.margins[1]
+    h = h - self.margins[2] - self.margins[3]
+    fbgui.App.logger.msg("DEBUG","x,y,w,h (%s childs): (%d,%d,%d,%d)" %
+                         (self._id,x,y,w,h))
+    
+    # now layout children
+    index = 0
+    for child in self._childs:
+      w_c, h_c = self._child_sizes[index]
+      y_c      = y
+
+      # horizontal alignment
+      if child.align[0] == fbgui.Widget.LEFT:
+        x_c = x
+      elif child.align[0] == fbgui.Widget.RIGHT:
+        x_c = x + self._child_w_max - w_c
+      else:
+        x_c = x + int((self._child_w_max - w_c)/2)
+
+      child._layout(x_c,y_c,w_c,h_c)
+      y     += h_c + self.padding[1]
+      index += 1
---
+++ demo/vbox.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Testprogram fbgui-vbox.
+#
+# The program tests the layout of various labels within a VBox.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pi-wstation
+#
+# ----------------------------------------------------------------------------
+
+import sys, os, datetime
+
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]),"../.."))
+
+import fbgui
+
+# --- global constants   -----------------------------------------------------
+
+FG_COLOR = fbgui.Color.BLACK
+BG_COLOR = fbgui.Color.WHITE
+
+FONT_SMALL  = 12
+FONT_MEDIUM = 24
+FONT_LARGE  = 48
+
+WIDTH  = 800
+HEIGHT = 600
+
+# ----------------------------------------------------------------------------
+
+def get_widgets():
+  """ create widget-tree """
+    
+  main = fbgui.Panel("main",
+                      settings=fbgui.Settings({'margins': (10,10,10,10)}),
+                      toplevel=True)
+  # add VBox
+  vbox = fbgui.VBox("vbox",
+                      settings=fbgui.Settings({
+                      'margins': 5,
+                      'padding': 2,
+                      'bg_color': fbgui.Color.SILVER,
+                      'align':    (fbgui.Widget.LEFT,fbgui.Widget.TOP),
+                      }))
+  main.add(vbox)
+
+  # add three texts
+  txt1 = fbgui.Label("id_txt1","this is",
+                      settings=fbgui.Settings({
+                      'font_size': FONT_SMALL,
+                      'bg_color' : fbgui.Color.RED080,
+                      'align':     fbgui.Widget.LEFT,
+                      }))
+  vbox.add(txt1)
+  txt2 = fbgui.Label("id_txt2","a small",
+                      settings=fbgui.Settings({
+                      'font_size': FONT_MEDIUM,
+                      'bg_color' : fbgui.Color.GREEN080,
+                      'align':     fbgui.Widget.CENTER,
+                      }))
+  vbox.add(txt2)
+  txt3 = fbgui.Label("id_txt1","and long text",
+                      settings=fbgui.Settings({
+                      'bg_color' : fbgui.Color.SILVER,
+                      'font_size': FONT_LARGE,
+                      'align':     fbgui.Widget.RIGHT,
+                      }))
+  vbox.add(txt3)
+  return main
+
+# ----------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+
+  config           = fbgui.Settings()
+  config.msg_level = "DEBUG"
+  config.bg_color  = BG_COLOR
+  config.font_size = FONT_MEDIUM
+  config.width     = WIDTH
+  config.height    = HEIGHT
+  config.title     = "Test VBox"
+
+  app   = fbgui.App(config)
+  panel = get_widgets()
+  panel.pack()
+
+  app.set_widget(panel)
+  app.run()
+  app.quit()
