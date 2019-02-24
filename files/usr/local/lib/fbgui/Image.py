f36ccf2dec7d60716e94af01fa73df414f5cc2e2
Bernhard Bablok <bablokb@gmx.de>
2019-02-24 17:03:51 +0100

    initial commit

---
+++ files/usr/local/lib/fbgui/Image.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# An image widget. The image is automatcally scaled to the requested size.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pygame-fbgui
+#
+# ----------------------------------------------------------------------------
+
+import pygame
+
+import fbgui
+
+class Image(fbgui.Widget):
+  """ draw an image """
+
+  # --- constructor   --------------------------------------------------------
+  
+  def __init__(self,id,img,settings=None,toplevel=False,parent=None):
+    """ constructor """
+
+    super(Image,self).__init__(id,settings=settings,
+                              toplevel=toplevel,parent=parent)
+    self._scale = getattr(settings,'scale',False)
+    self._img = img
+    self.set_image(img,refresh=False)
+
+  # --- set the image of this Image   ----------------------------------------
+
+  def set_image(self,img,font=None,refresh=True):
+    """ set the image of the Image """
+
+    if img == self._img:
+      return
+    fbgui.App.logger.msg("TRACE","changing image of Image %s" % self._id)
+
+    if img:
+      self._raw_surface = pygame.image.load(img).convert()
+      self._rect = self._raw_surface.get_rect()
+    else:
+      self._raw_surface = None
+      self._rect        = None
+
+    self._scaled_surface = self._raw_surface
+    self._scaled_rect    = self._rect
+    self._img = img
+
+    if refresh:
+      if not self._scale:
+        # post a layout-event
+        fbgui.App.logger.msg("TRACE","posting layout-event from %s" % self._id)
+        event = pygame.fastevent.Event(fbgui.EVENT,
+                                       code=fbgui.EVENT_CODE_LAYOUT,
+                                       widget=self)
+        pygame.fastevent.post(event)
+      else:
+        # only content changed, so post a redraw-event
+        fbgui.App.logger.msg("TRACE","posting redraw-event from %s" % self._id)
+        event = pygame.fastevent.Event(fbgui.EVENT,
+                                       code=fbgui.EVENT_CODE_REDRAW,
+                                       widget=self)
+        pygame.fastevent.post(event)
+      
+  # --- query minimum size   -------------------------------------------------
+
+  def _calc_minimum_size(self,w,h):
+    """ query minimum size of widget """
+
+    if self._is_size_valid:
+      return
+
+    from_parent_w,from_parent_h = self._set_size_from_parent(w,h)
+    if not self._img:
+      self._is_size_valid = True
+      return
+    elif self._scale:
+      self.w_min = 1
+      self.h_min = 1
+      self._is_size_valid = True
+      return
+      
+    if not from_parent_w:
+      self.w_min = self._rect.w
+    if not from_parent_h:
+      self.h_min = self._rect.h
+
+    fbgui.App.logger.msg("TRACE","min_size (%s): (%d,%d)" %
+                                             (self._id,self.w_min,self.h_min))
+    self._is_size_valid = True
+
+  # --- layout widget   ------------------------------------------------------
+
+  def _layout(self,x,y,w,h):
+    """ layout widget:
+          x,y: target-position as defined by the parent
+          w,h: width and height of parent (adapted by e.g. margins)
+    """
+
+    # call the standard-layout mechanism
+    self._std_layout(x,y,w,h)
+
+    # if we are scalable, use argument w,h
+    if self._scale:
+      self.screen.w = w
+      self.screen.h = h
+    else:
+      self.screen.w = self.w_min
+      self.screen.h = self.h_min
+
+    fbgui.App.logger.msg("TRACE","layout (%s): (%d,%d,%d,%d)" %
+           (self._id,self.screen.x,self.screen.y,self.screen.w,self.screen.h))
+
+  # --- redraw widget   ------------------------------------------------------
+
+  def draw(self):
+    """ draw the widget """
+
+    if not self._img:
+      return
+
+    # scale image to new dimension if necessary
+    if not (self.screen.w == self._scaled_rect.w and
+        self.screen.h == self._scaled_rect.h):
+      self._scaled_surface = (
+        pygame.transform.scale(self._raw_surface,(self.screen.w,self.screen.h)))
+      self._scaled_rect = self._scaled_surface.get_rect()
+
+    # blit the image to the target position
+    fbgui.App.display.screen.blit(self._scaled_surface,
+                                    (self.screen.x,self.screen.y))
---
+++ files/usr/local/lib/fbgui/demo/images.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Testprogram for images. The program expects a list of images on the
+# commandline.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/pygame-fbgui
+#
+# ----------------------------------------------------------------------------
+
+import sys, os, datetime, threading, signal
+import pygame
+
+sys.path.append(os.path.join(os.path.dirname(sys.argv[0]),"../.."))
+
+import fbgui
+
+# --- global constants   -----------------------------------------------------
+
+FG_COLOR = fbgui.Color.BLACK
+BG_COLOR = fbgui.Color.WHITE
+
+FONT_SMALL  = 12
+FONT_MEDIUM = 24
+FONT_LARGE  = 48
+
+WIDTH  = 800
+HEIGHT = 600
+
+# ----------------------------------------------------------------------------
+
+class MyApp(fbgui.App):
+  """ subclass of App for this application """
+
+  # -------------------------------------------------------------------------
+
+  def __init__(self,images,settings=fbgui.Settings()):
+    """ constructor """
+
+    super(MyApp,self).__init__(settings=settings)
+    self._images     = images
+    self._stop_event = threading.Event()
+
+    panel = self.get_widgets()
+    panel.pack()
+    self.set_widget(panel)
+
+  # -------------------------------------------------------------------------
+
+  def get_widgets(self):
+    """ create widget-tree """
+    
+    main = fbgui.Panel("main",
+                      settings=fbgui.Settings({'margins': (10,10,10,10)}),
+                      toplevel=True)
+    # add Image
+    self._img_widget = fbgui.Image("img",None,
+                      settings=fbgui.Settings({
+                        'Xwidth': 600,
+                        'Xheight': 200,
+                        'scale': False,             
+                        'align': (fbgui.CENTER,fbgui.CENTER),
+                      }),parent=main)
+    return main
+
+  # -------------------------------------------------------------------------
+
+  def update(self):
+    n = -1
+    total = len(self._images)
+    while True:
+      n = (n+1) % total
+      self.logger.msg("DEBUG","image: %s" % self._images[n])
+      self._img_widget.set_image(self._images[n],refresh=True)
+      if self._stop_event.wait(3):
+        break
+
+  # -----------------------------------------------------------------------
+
+  def on_start(self):
+    """ override base-class on_start-method """
+    # setup async-thread
+    update_thread = threading.Thread(target=myapp.update)
+    update_thread.start()
+
+  # -----------------------------------------------------------------------
+
+  def on_quit(self):
+    """ override base-class quit-method """
+
+    super(MyApp,self).on_quit()
+    self._stop_event.set()
+    sys.exit(0)
+
+  # ----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+  config           = fbgui.Settings()
+  config.msg_level = "TRACE"
+  config.bg_color  = BG_COLOR
+  config.font_name = "couriernew"
+  config.font_size = FONT_LARGE
+  config.width     = WIDTH
+  config.height    = HEIGHT
+  config.title     = "Test Events"
+
+  myapp = MyApp(sys.argv[1:],config)
+  myapp.run()
