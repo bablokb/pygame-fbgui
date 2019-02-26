e3ca49e78bc38c19b19ebf4663120e80c720a1f6
Bernhard Bablok <bablokb@gmx.de>
2019-02-26 16:16:55 +0100

    implement default handlers for mouse-events

--- files/usr/local/lib/fbgui/Button.py
+++ files/usr/local/lib/fbgui/Button.py
@@ -15,6 +15,10 @@ import fbgui
 class Button(fbgui.HBox):
   """ Horizontal box """
 
+  NORMAL = 0
+  HOVER  = 1
+  DOWN   = 2
+
   # --- constructor   --------------------------------------------------------
   
   def __init__(self,id,img=None,text=None,
@@ -26,12 +30,22 @@ class Button(fbgui.HBox):
     super(Button,self).__init__(id,settings=settings,
                               toplevel=toplevel,parent=parent)
 
+    # add default handlers
+    if not self._on_mouse_motion:
+      self._on_mouse_motion = self.on_mouse_motion
+    if not self._on_mouse_btn_down:
+      self._on_mouse_btn_down = self.on_mouse_btn_down
+    if not self._on_mouse_btn_up:
+      self._on_mouse_btn_up = self.on_mouse_btn_up
 
     self._img = None     # image filename
     self._Image = None   # Image widget
 
     self._text  = None   # button text
     self._Label = None   # Label widget
+
+    # state of button
+    self._state = Button.NORMAL
 
     self.set_image(img,refresh=False)
     self.set_text(text,refresh=False)
@@ -77,3 +91,35 @@ class Button(fbgui.HBox):
                                   settings=settings,parent=self)
 
     self._text = text
+
+  # --- handle mouse-motion events    ----------------------------------------
+
+  def on_mouse_motion(self,event):
+    """ handle mouse-motion events """
+
+    if self._draw_rect.collidepoint(event.pos):
+      fbgui.App.logger.msg("TRACE","on_mouse_motion for Button %s" % self._id)
+      self._state = Button.HOVER
+    else:
+      self._state = Button.NORMAL
+    return False
+
+  # --- handle mouse-button down events    ----------------------------------
+
+  def on_mouse_btn_down(self,event):
+    """ handle mouse-button down events """
+
+    if self._draw_rect.collidepoint(event.pos):
+      fbgui.App.logger.msg("TRACE","on_mouse_btn_down for Button %s" % self._id)
+      self._state = Button.DOWN
+    return False
+
+  # --- handle mouse-button up events    ------------------------------------
+
+  def on_mouse_btn_up(self,event):
+    """ handle mouse-button up events """
+
+    if self._draw_rect.collidepoint(event.pos):
+      fbgui.App.logger.msg("TRACE","on_mouse_btn_up for Button %s" % self._id)
+      self._state = Button.NORMAL
+    return False
