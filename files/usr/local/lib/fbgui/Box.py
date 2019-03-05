f99f1ffd7d33d70505e6fb3983d6a8a3628afe1e
Bernhard Bablok <bablokb@gmx.de>
2019-03-05 15:52:09 +0100

    _calc_minimum_size(): pass size minus margins to children

--- files/usr/local/lib/fbgui/Box.py
+++ files/usr/local/lib/fbgui/Box.py
@@ -62,7 +62,8 @@ class Box(fbgui.Panel):
 
     # calculate size of all children
     for child in self._childs:
-      child._calc_minimum_size(self.w_min,self.h_min)
+      child._calc_minimum_size(self.w_min-self.margins[0]-self.margins[1],
+                               self.h_min-self.margins[2]-self.margins[3])
       (c_w,c_h) = child.w_min, child.h_min
       self._child_w_max  = max(c_w,self._child_w_max)
       self._child_h_max  = max(c_h,self._child_h_max)
