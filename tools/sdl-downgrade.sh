1e6929685ca9d1fce3337c25f35945dae3dd13f3
Bernhard Bablok <bablokb@gmx.de>
2019-07-19 17:57:38 +0200

    updated to new repository-site for wheezy

--- tools/sdl-downgrade.sh
+++ tools/sdl-downgrade.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 # --------------------------------------------------------------------------
 # In order to run tslib with pygame, a downgrade to an older version
-# is currently (Stretch) necessary.
+# is currently necessary.
 #
 # source: 
 # https://learn.adafruit.com/adafruit-2-4-pitft-hat-with-resistive-touchscreen-mini-kit/pitft-pygame-tips
+#
+# Note that this is a modified version:
+#   - wheezy is now moved to legacy.raspbian.org
+#   - the script not only works with stretch, but with all versions
 #
 # Website: https://github.com/bablokb/pygame-fbgui
 #
 # --------------------------------------------------------------------------
 
 # enable wheezy package sources
-echo "deb http://archive.raspbian.org/raspbian wheezy main
+echo "deb http://legacy.raspbian.org/raspbian wheezy main
 " > /etc/apt/sources.list.d/wheezy.list
 
-# set stable as default package source (currently stretch)
+# set stable as default package source
 echo "APT::Default-release \"stable\";
 " > /etc/apt/apt.conf.d/10defaultRelease
 
-# set the priority for libsdl from wheezy higher then the stretch package
+# current release
+current=$(grep -E '^deb' /etc/apt/sources.list | cut -d' ' -f3)
+
+# set the priority for libsdl from wheezy higher then the current package
 echo "Package: libsdl1.2debian
-Pin: release n=stretch
+Pin: release n=$current
 Pin-Priority: -10
 Package: libsdl1.2debian
 Pin: release n=wheezy
